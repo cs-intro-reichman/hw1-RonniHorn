a628b36ef2ba4031fde37cdbfc1bb1fc4be297a3
RonniHorn <ronni.horn@post.runi.ac.il>
2025-10-31 14:57:56 +0200

    TimeForma

--- TimeFormat.java
+++ TimeFormat.java
@@ -1,8 +1,9 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
+
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-		int minutes = Integer.parseInt("" + args[0].charAt(2) + args[0].charAt(3));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         if(hours > 12){
 			System.out.print(hours-12);
 			if(minutes<10)
