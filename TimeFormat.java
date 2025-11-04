ecd54dba036c35604758772d0fed54d25554baf2
RonniHorn <ronni.horn@post.runi.ac.il>
2025-11-04 20:00:26 +0200

    TimeForma

--- TimeFormat.java
+++ TimeFormat.java
@@ -4,28 +4,29 @@ public class TimeFormat {
 
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        if(hours > 12){
-			System.out.print(hours-12);
-			if(minutes<10)
-				System.out.print(":0" + minutes + " PM");
-			else
-				System.out.print(":" + minutes + " PM");
-		}
-		else{
-			System.out.print(hours);
-			if(hours == 00)
+        if(hours >= 12){
+			if(hours == 12)
 			{
+				System.out.print(hours);
 				if(minutes<10)
 					System.out.print(":0" + minutes + " PM");
 				else
 					System.out.print(":" + minutes + " PM");
 			}
 			else{
+				System.out.print(hours-12);
 				if(minutes<10)
+					System.out.print(":0" + minutes + " PM");
+				else
+					System.out.print(":" + minutes + " PM");
+			}	
+		}
+		else{
+			System.out.print(hours);
+			if(minutes<10)
 					System.out.print(":0" + minutes + " AM");
 				else
 					System.out.print(":" + minutes + " AM");
-			}
 		}
 			
 	}
